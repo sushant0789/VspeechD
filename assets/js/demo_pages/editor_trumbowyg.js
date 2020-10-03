257de201a232de0d938b2ec5cc91b07a6dfb1e53
sushant0789 <53333023+sushant0789@users.noreply.github.com>
2020-10-03 14:18:11 +0530

    first

--- assets/js/demo_pages/editor_trumbowyg.js
+++ assets/js/demo_pages/editor_trumbowyg.js
@@ -6,95 +6,90 @@
  *
  * ---------------------------------------------------------------------------- */
 
-
 // Setup module
 // ------------------------------
 
-var Trumbowyg = function() {
+var Trumbowyg = (function () {
+  //
+  // Setup module components
+  //
 
-
-    //
-    // Setup module components
-    //
-
-    // Trumbowyg editor
-    var _componentTrumbowyg = function() {
-        if (!$().trumbowyg) {
-            console.warn('Warning - trumbowyg.min.js is not loaded.');
-            return;
-        }
-
-        // Set custom icons path
-        $.trumbowyg.svgPath = '../../../../global_assets/js/plugins/editors/trumbowyg/ui/icons.svg';
-
-        // Default initialization
-        $('#trumbowyg_default').trumbowyg();
-
-        // Custom toolbar
-        $('#trumbowyg_custom').trumbowyg({
-            btns: [
-                // ['formatting'],
-                ['strong', 'em'],
-                ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
-                ['unorderedList', 'orderedList'],
-                ['undo', 'redo'], // Only supported in Blink browsers
-                ['insertImage', 'link'],
-                ['viewHTML'],
-                ['fullscreen']
-            ]
-        });
-
-        // Custom plugins
-        $('#trumbowyg_plugins').trumbowyg({
-            btns: [
-                ['base64'],
-                ['foreColor', 'backColor'],
-                ['insertAudio'],
-                ['noembed'],
-                ['preformatted'],
-                ['template'],
-                ['upload'],
-                ['fullscreen']
-            ],
-            plugins: {
-                templates: [
-                    {
-                        name: 'Template 1',
-                        html: '<p>I am a template!</p>'
-                    },
-                    {
-                        name: 'Template 2',
-                        html: '<p>I am a different template!</p>'
-                    }
-                ],
-                upload: {
-                    serverPath: '',
-                    fileFieldName: 'image',
-                    headers: {
-                        '': ''
-                    },
-                    urlPropertyName: ''
-                }
-            }
-        });
-    };
-
-
-    //
-    // Return objects assigned to module
-    //
-
-    return {
-        init: function() {
-            _componentTrumbowyg();
-        }
+  // Trumbowyg editor
+  var _componentTrumbowyg = function () {
+    if (!$().trumbowyg) {
+      console.warn("Warning - trumbowyg.min.js is not loaded.");
+      return;
     }
-}();
 
+    // Set custom icons path
+    $.trumbowyg.svgPath = "assets/js/plugins/editors/trumbowyg/ui/icons.svg";
+
+    // Default initialization
+    $("#trumbowyg_default").trumbowyg();
+
+    // Custom toolbar
+    $("#trumbowyg_custom").trumbowyg({
+      btns: [
+        // ['formatting'],
+        ["strong", "em"],
+        ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull"],
+        ["unorderedList", "orderedList"],
+        ["undo", "redo"], // Only supported in Blink browsers
+        ["insertImage", "link"],
+        ["viewHTML"],
+        ["fullscreen"],
+      ],
+    });
+
+    // Custom plugins
+    $("#trumbowyg_plugins").trumbowyg({
+      btns: [
+        ["base64"],
+        ["foreColor", "backColor"],
+        ["insertAudio"],
+        ["noembed"],
+        ["preformatted"],
+        ["template"],
+        ["upload"],
+        ["fullscreen"],
+      ],
+      plugins: {
+        templates: [
+          {
+            name: "Template 1",
+            html: "<p>I am a template!</p>",
+          },
+          {
+            name: "Template 2",
+            html: "<p>I am a different template!</p>",
+          },
+        ],
+        upload: {
+          serverPath: "",
+          fileFieldName: "image",
+          headers: {
+            "": "",
+          },
+          urlPropertyName: "",
+        },
+      },
+    });
+  };
+
+  //
+  // Return objects assigned to module
+  //
+
+  return {
+    init: function () {
+      _componentTrumbowyg();
+    },
+  };
+})();
 
 // Initialize module
 // ------------------------------
 
-document.addEventListener('DOMContentLoaded', function() {
-    Trumbowyg.init();
+document.addEventListener("DOMContentLoaded", function () {
+  Trumbowyg.init();
 });
